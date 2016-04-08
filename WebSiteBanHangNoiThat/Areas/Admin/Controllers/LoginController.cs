f87c9f7ce051c60c6ceda0296493cbab7ee0ba0b
netnanetn <netnanetn0704@gmail.com>
2016-04-08 17:33:23 +0700

    init

---
+++ WebSiteBanHangNoiThat/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHangNoiThat.Models;
+using WebSiteBanHangNoiThat.DataBaseModels;
+using System.Web.Security;
+
+namespace WebSiteBanHangNoiThat.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        web_interiorEntities db = new web_interiorEntities();
+        
+        // GET: Admin/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckLogin(DangNhapViewModel taikhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                //cấu trúc linq kiểm tra xem có tài khoản nào khớp hay không
+                var checkLogin = db.Users.Any(s => s.Name == taikhoan.UserLogin && s.PassWordHash == taikhoan.Password);
+                //nếu có checklogin = true
+                if (checkLogin)
+                {
+                    FormsAuthentication.SetAuthCookie(taikhoan.UserLogin, false);
+                     return RedirectToAction("Index", "Home");
+                  
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu, kiểm tra lại nhé!");
+                    //trở lại trang đăng nhập, báo lỗi
+                    return View("Login", taikhoan);
+                }
+            }
+            //Đăng nhập thành công, trở về trang chủ
+            return Redirect("/");
+        }
+
+        public ActionResult LogOut()
+        {
+            //Đăng xuất khỏi ứng dụng
+            FormsAuthentication.SignOut();
+            //Về trang chủ
+            return Redirect("/");
+        }
+    }
+}
---
+++ WebSiteBanHangNoiThat/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSiteBanHangNoiThat.DataBaseModels;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Security;
+
+namespace WebSiteBanHangNoiThat.Models
+{
+
+    public class TaiKhoanViewModel
+    {
+    }
+    public class DangNhapViewModel
+    {
+        [Required]
+        [Display(Name = "UserLogin")]
+        public string UserLogin { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+    public class DangKyViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "UserLogin")]
+        public string UserLogin { get; set; }
+
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "ConfirmPassword")]
+        [Compare("Password", ErrorMessage = "Mật khẩu không trùng khớp")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class PhanQuyenProvider : RoleProvider
+    {
+        public override string[] GetRolesForUser(string namelogin)
+        {
+            using (web_interiorEntities db = new web_interiorEntities())
+            {
+                var taikhoan = db.Users.FirstOrDefault(x => x.Name == namelogin);
+                if (taikhoan == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    string[] listQuyen = taikhoan.Roles.Select(x => x.Name).ToArray();
+                    return listQuyen;
+                }
+            }
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
